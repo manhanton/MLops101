af998b73a1735075bdfff26b77bc0a550e145bcb
mac-m1-yothin <yo_scorpien007@hotmail.com>
2022-08-27 12:44:15 +0700

    The first model trained with sci-kit learn

---
+++ read_data.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+def read_dataframe(filename):
+    if filename.endswith('.csv'):
+        df = pd.read_csv(filename)
+
+        df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
+        df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+    elif filename.endswith('.parquet'):
+        df = pd.read_parquet(filename)
+
+    df['duration'] = df.lpep_dropoff_datetime - df.lpep_pickup_datetime
+    df.duration = df.duration.apply(lambda td: td.total_seconds() / 60)
+
+    df = df[(df.duration >= 1) & (df.duration <= 60)]
+
+    categorical = ['PULocationID', 'DOLocationID']
+    df[categorical] = df[categorical].astype(str)
+    
+    return df
